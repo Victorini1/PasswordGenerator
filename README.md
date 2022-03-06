6d64175ea57dc432ab1f1b4a646d98bd39e4563e
Baeonetta <victor.m.kennedy@gmail.com>
2022-03-06 17:18:33 -0600

    created more funcitonality and added readME

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # PasswordGenerator
+
+## Objectives
+
+This assignment was meant to create the javascript code to create a randomized password generator that wrote an individualized password to the user interface in the browser. By using functions and for loops, I was able to have seperate arrays merge and have individual items from those arrays randomized to create a brand new string of characters to be used in the password. The user is able to select the length preferred and allow which type of characters to include to the password.
+
+
+## Screenshot of Page
+
+![Password Generator Page](./Assets/Password%20Generator.png)
+
+## Link to Page
+
+[Password Generator](https://victorini1.github.io/PasswordGenerator/)
+
+#### Collaborators
+
+I worked with some of the cohort to get some things funcioning as I intended.
+
+- Eugene Ahn
+- Neema Velasco
--- script.js
+++ script.js
@@ -12,66 +12,66 @@ var passwordCharacterCount;
 // Set up valid characters for generator to populate
 var uppercaseChars = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
 var lowercaseChars = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "|", "\\", "}", "{", "]", "[", '"', "'", ":", ";", "/", "?", ".", ">", ",", "<", "`", "~"];
-var characters = []
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*"];
+var characters = [];
 
-
+// Provided by instructor
 
 // Ask how many characters the user would like to add
 function userInput(){
-  function askAboutLength(passwordCharacterCount){
+  function askAboutLength(){
     passwordCharacterCount = parseInt(prompt("How many characters would you like the password to be? Please choose between 8 - 124 characters long."));
     return passwordCharacterCount
   }
-
+  
   function askAboutUppercase(){
   allowUppercase = confirm("Would like upper case characters?");
-  // If yes then push uppercase to the characters array
+   // If yes then push  single uppercase to the characters array
   if (allowUppercase === true) {
-    characters.push(uppercaseChars.join('')); //join arrays cited from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+    characters.push(...uppercaseChars); //spread operators cited from  https://www.educative.io/edpresso/what-is-the-spread-operator-in-javascript
     }
   }
 
   function askAboutLowercase(){
     allowLowerCase = confirm("Would you like lower case characters?");
-  // If yes then push lowercase to the characters array
+  // If yes then push single lowercase to the characters array
     if (allowLowerCase === true) {
-    characters.push(lowercaseChars.join('')); 
+    characters.push(...lowercaseChars); 
     }
   }
   function askAboutNumber() {
     allowNumbers = confirm("Would you like numbers?");
-  // If yes then push numbers to the characters array
+  // If yes then push singular numbers to the characters array
     if (allowNumbers === true) {
-    characters.push(number.join('')); 
+    characters.push(...numbers); 
     }
   }
 
   function askAboutSpecials(){
     allowSpecials = confirm("Would you like special characters?");
-    // If yes then push special chracters to the characters array
-    if (allowSpecials === true) {
-      characters.push(number.join('')); 
+    // If yes then push singular special chracters to the characters array
+    if (allowNumbers === true) {
+      characters.push(...specialCharacters); 
     }
   }
-//Ask the questions
- askAboutLength()
- askAboutUppercase()
- askAboutLowercase()
- askAboutNumber()
- askAboutSpecials()
-}
+  //Ask the questions
+   askAboutLength();
+   askAboutUppercase();
+   askAboutLowercase();
+   askAboutNumber();
+   askAboutSpecials();
+  }
 
-// If upper case characters allowed push them to the array
+
 // Repeat for other criteria; for the number of characters, use a prompt statement. Google it. NOTE: prompt statements think any value you provide is a string. You will need to convert it to a number.
 // Once all the criteria are determined, this function will generate the password. You can create other functions also if you need them.
 function generatePassword(){
   userInput();
-  var finalResult = ""
+  var finalResult = "";
 //Puts all the characters into an array and loop that over and over again until it gets the amount of characters requested
-  for (var i=1; i<=passwordCharacterCount; i++ ) {
-  var randomized = Math.floor(Math.random() * characters.length -1)
+  for (var i=0; i<= passwordCharacterCount; i++ ) {
+  var randomized = Math.floor(Math.random() * characters.length)
   finalResult += characters[randomized]
   }
   return finalResult;
@@ -79,6 +79,11 @@ function generatePassword(){
 
 // Write password to the #password input
 function writePassword() {
+
+
+
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
